1f50dfd1ac091d37348af5c32ba15dfe3660d9ea
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-21 00:44:29 +0500

    Setup project for Action Cable implementation
    * Added redis to gemfile
    * Genereted room_channel
    * Generated pages_controller
    * Changed development adapter to redis
    * Added routes for pages

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,17 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    console.log('Connected to Room Channel')
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    console.log('Disconnected from Room Channel')
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
